395d9035f5f53b7b583a2dc3e603daed4e7a8d7e
H.Y. Leung <hy.leung@gmail.com>
2013-03-16 14:46:17 -0700

    Adds some hiera configuration.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,3 +1,15 @@
-node jbossdev {
+node  jbossdev {
+    include jbossdev::configure
+}
 
+
+class jbossdev::configure {
+    file {
+        "/etc/puppet/hiera.yaml":
+            source => "/vagrant/files/hiera.yaml";
+        "/etc/puppet/common.yaml":
+            source => "/vagrant/files/common.yaml";            
+        "/etc/puppet/jboss-config.yaml":
+            source => "/vagrant/files/jboss-config.yaml";
+    } 
 }
